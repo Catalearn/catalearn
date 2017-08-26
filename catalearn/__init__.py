5a7fe157dedcae388a51f67f10a2eb362a63fc1b
yl573 <yl573@cam.ac.uk>
2017-08-26 21:54:14 +0800

    added autoupdate check

--- catalearn/__init__.py
+++ catalearn/__init__.py
@@ -1,8 +1,14 @@
 import sys
 import warnings
+from .upgrade import isLatestVersion
 
 sys.setrecursionlimit(50000)
 warnings.filterwarnings('ignore')
+
+if not isLatestVersion():
+    print('This version of catalearn is no longer compatible with the backend')
+    print('Please use \'pip3 install -U catalearn\' to upgrade to the latest version')
+    sys.exit()
 
 from .catalearn import *
 
